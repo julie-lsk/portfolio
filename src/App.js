2c4fd2948bdc4e1e40174017ed930fcc05941112
Julie Lausecker <julielausecker@gmail.com>
2024-07-02 13:48:13 +0200

    First commit - version Vanta.js bg + avant grandes modifs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import * as THREE from 'three';
+import FOG from 'vanta/dist/vanta.fog.min';
+import '../src/app.scss';
 
-function App() {
+import Accueil from "./pages/accueil/Accueil";
+import Header from "./components/header/Header";
+
+
+/********** Animation Vanta - background **********/
+const VantaAnimation = () => {
+  const [vantaEffect, setVantaEffect] = useState(null)
+  
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(FOG({
+        el: "#vanta",
+        THREE: THREE,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+
+        // Rouge : 
+        // highlightColor: 0xcf1d1d,
+        // midtoneColor: 0xac052d,
+        // lowlightColor: 0xc70202,
+        // baseColor: 0x680128,
+
+        // Bleu :
+        highlightColor: 0xb6edda,
+        midtoneColor: 0x67d2aa,
+        lowlightColor: 0xbeffde,
+        baseColor: 0x4b9aac,
+
+        blurFactor: 0.53,
+        zoom: 1,
+        speed: 1.1,
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect]) 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id='vanta'></div>
+  )
+}
+
+
+
+function App() 
+{
+  return (
+    <Router>
+
+      <VantaAnimation />
+
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Accueil />}></Route>
+      </Routes>
+
+      {/* <Footer /> */}
+
+    </Router>
   );
 }
 
---
+++ src/pages/accueil/Accueil.jsx
@@ -0,0 +1,21 @@
+import "./accueil.scss";
+
+function Accueil()
+{
+    return (
+        <main>
+
+            <section className="accueil">
+
+            <h1>Julie LAUSECKER <br></br>
+            Développeuse Web</h1>
+
+            <h2>Portfolio</h2>
+
+            </section>
+
+        </main>
+    )
+}
+
+export default Accueil
